3277957cf395d0ff6612be487f3d9c8690c33ff9
Divine Collins <onyedivine14@gmail.com>
2026-04-29 22:55:37 +0100

    add BlockType enum for Markdown block types

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,14 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
